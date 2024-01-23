5846838f332554f3f029682fc26015b9999c75ca
salvacovelo <salvacovelom@gmail.com>
2024-01-23 14:44:38 -0400

    added static and setting

--- glik/urls.py
+++ glik/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +28,5 @@ urlpatterns = [
     path('api/lease/', include('leases.urls')),
     path('api/payment/', include('payments.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
